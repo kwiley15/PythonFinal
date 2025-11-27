7842de03bfcd9271505bf88f59e26c59487bb166
kristopher L Wiley <kriswiley0421@gmail.com>
2025-11-27 00:26:22 -0700

    the middle of the end

--- netflixsite/netflixsite/asgi.py
+++ netflixsite/netflixsite/asgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
-import os
+import os # import os to interact with enviroment variables 
 
-from django.core.asgi import get_asgi_application
+from django.core.asgi import get_asgi_application # Import ASGI application handler
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'netflixsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'netflixsite.settings') # settings module for the netflixsite project, which settings.py to use 
 
-application = get_asgi_application()
+application = get_asgi_application() # create the application 
+
+#we are not using this page 
--- netflixsite/netflixsite/wsgi.py
+++ netflixsite/netflixsite/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
+import os # import os to interact with enviroment variables 
 
-from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application # import WSGI application handler 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'netflixsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'netflixsite.settings') # set the default settings for module , which settings.py to use 
 
-application = get_wsgi_application()
+application = get_wsgi_application() # create the application 
+
+#we are not using this page 
